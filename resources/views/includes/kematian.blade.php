f46c0a502f63f6538447b5d46167ae0400b87eab
fellllll <c14220027@john.petra.ac.id>
2024-09-16 21:00:51 +0700

    bikin base webnya sm inherittance sm link linknyaa

---
+++ resources/views/includes/kematian.blade.php
@@ -0,0 +1,8 @@
+@extends('base.base')
+
+@section('content')
+
+<div class="container m-auto">
+        <h1 class="text-center font-bold mt-10 text-4xl">LAYANAN KEMATIAN</h1>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('includes.home');
 });
-Route::get('/test', function () {
-    return view('app');
+
+// Route::get('/test', function () {
+//     return view('app');
+// });
+
+Route::get('/informasi', function () {
+    return view('includes.informasi');
+});
+
+Route::get('/renungan', function () {
+    return view('includes.renungan');
+});
+
+Route::get('/diakonia', function () {
+    return view('includes.diakonia');
+});
+
+Route::get('/pernikahan', function () {
+    return view('includes.pernikahan');
+});
+
+Route::get('/kematian', function () {
+    return view('includes.kematian');
 });
