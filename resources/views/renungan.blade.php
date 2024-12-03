b6e9f23358fdb3df74f83a1ffbd8537ef166a5bb
euginiagabrielle <icin2004@gmail.com>
2024-12-04 00:29:01 +0700

    rapiin ui renungan + benerin href navbar user

--- resources/views/renungan.blade.php
+++ resources/views/renungan.blade.php
@@ -10,20 +10,19 @@
         @foreach ($renungans as $renungan)
 
         <div class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl w-80 flex flex-col">
-            <div class = "w-full flex justify-center">
+            <div class = "w-full h-1/2 flex justify-center">
                 <img src="{{ asset('storage/'. $renungan->poster) }}">
             </div>
 
-            <h5 class="text-gray-200 font-bold mb-3 mt-1">{{ $renungan->judul }}</h5>
-            <h6 class="text-gray-200 font-bold mb-1">Renungan Oleh: {{ $renungan->pembuat }}</h6>
-            <p class="text-center text-gray-200">
+            <h5 class="text-slate-700 font-bold text-xl mt-2 py-2">{{ $renungan->judul }}</h5>
+            <h6 class="text-slate-700 font-bold my-1">Renungan Oleh: {{ $renungan->pembuat }}</h6>
+            <p class="text-center text-slate-500 my-1">
                 {{ Str::limit($renungan->isiRenungan, 200) }}
             </p>
             
-            <button>
-                <br>
-                <a href="/renungan/{{$renungan->id  }}" class="rounded-md bg-indigo-500 px-3 py-3 text-sm font-semibold text-white">Klik untuk membaca!</a>
-            </button>
+            <div class="mt-auto pt-5 flex items-center justify-center">
+                <a href="/renungan/{{$renungan->id  }}" class="rounded-md bg-indigo-500 hover:bg-indigo-600 px-3 py-3 text-sm font-semibold text-white">Klik untuk membaca!</a>
+            </div>
         </div>
         @endforeach
     </div>
