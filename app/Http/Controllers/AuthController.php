a8a1009d028b86e30a3682bc23c7b92d8049a3ae
MaxGreg10 <maximillianus2004@gmail.com>
2024-12-04 19:56:25 +0700

    fix error login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,7 +36,7 @@ class AuthController extends Controller
         if(Auth::attempt($credentials)){
             
             $request->session()->regenerate();
-            return redirect()->intended(route(" index"));
+            return redirect()->route('admin.index');
         }
         return back()->with('loginError', 'login failed!');
     }
