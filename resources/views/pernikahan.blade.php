f781a97571c53e2ad356ca500e3a0a2d0c25fae7
euginiagabrielle <icin2004@gmail.com>
2024-12-04 21:29:58 +0700

    hapus centang informasi pernikahan + rapiin informasi event

--- resources/views/pernikahan.blade.php
+++ resources/views/pernikahan.blade.php
@@ -199,10 +199,10 @@
         </div>
 
 
-        <div class="flex items-center my-4">
+        <!-- <div class="flex items-center my-4">
           <input id="default-checkbox" name="allow" type="checkbox" value="yes" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
           <label for="default-checkbox" class="ms-2 text-sm font-semibold text-red-600 dark:text-red-600">Menampilkan informasi pernikahan di halaman informasi</label>
-        </div>
+        </div> -->
 
 
     <!-- submit button -->
